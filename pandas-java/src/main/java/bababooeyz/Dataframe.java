a97cbd0a7af787ef01cf6c8dc6d6ac20f0e48626
Matthieu Rua <matthieu.rua@etu.univ-grenoble-alpes.fr>
2025-03-19 08:43:40 +0100

    refractor: rename default classes to Dataframe and DatarfameTest

--- pandas-java/src/main/java/bababooeyz/Dataframe.java
+++ pandas-java/src/main/java/bababooeyz/Dataframe.java
@@ -3,7 +3,7 @@ package bababooeyz;
 /**
  * Hello world!
  */
-public class App {
+public class Dataframe {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- pandas-java/src/test/java/bababooeyz/DataframeTest.java
+++ pandas-java/src/test/java/bababooeyz/DataframeTest.java
@@ -1,13 +1,12 @@
 package bababooeyz;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class DataframeTest {
 
     /**
      * Rigorous Test :-)
