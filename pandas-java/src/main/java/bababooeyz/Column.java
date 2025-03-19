b287588d168f9533f153ac37ef7a5f776c05bb57
Matthieu Rua <matthieu.rua@etu.univ-grenoble-alpes.fr>
2025-03-19 09:33:07 +0100

    docs: Added simple documentation to the Column and Dataframe class

--- pandas-java/src/main/java/bababooeyz/Column.java
+++ pandas-java/src/main/java/bababooeyz/Column.java
@@ -3,17 +3,34 @@ package bababooeyz;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a column in a table-like structure, storing values of a specific type.
+ * 
+ * @param <T> The type of the values stored in the column.
+ */
 public class Column<T> {
-    private String name;
-    private Class<T> type;
-    private List<T> values;
+    private String name; // The name of the column.
+    private Class<T> type; // The type of the values stored in the column.
+    private List<T> values; // The list of values stored in the column.
 
+    /**
+     * Constructs a new Column with the specified name and type.
+     * 
+     * @param name The name of the column.
+     * @param type The class type of the values stored in the column.
+     */
     public Column(String name, Class<T> type){
         this.name = name;
         this.type = type;
         values = new ArrayList<>();
     }
 
+    /**
+     * Adds a value to the column. Ensures the value is of the correct type.
+     * 
+     * @param value The value to add to the column.
+     * @throws IllegalArgumentException If the value is not of the correct type.
+     */
     public void add(T value){
         if(value != null && !type.isInstance(value)){
             throw new IllegalArgumentException("Incorrect type for column "+ name);
@@ -21,6 +38,12 @@ public class Column<T> {
         values.add(value);
     }
 
+    /**
+     * Retrieves the value at the specified index in the column.
+     * 
+     * @param index The index of the value to retrieve.
+     * @return The value at the specified index.
+     */
     public T getValue(int index){
         return values.get(index);
     }
--- pandas-java/src/main/java/bababooeyz/Dataframe.java
+++ pandas-java/src/main/java/bababooeyz/Dataframe.java
@@ -1,12 +1,24 @@
 package bababooeyz;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * The Dataframe class represents the Dataframe type used in the pandas library.
+ * It is a two-dimensional, with axes labeled rows and columns.
+ */
 public class Dataframe {
-    private Map <Integer, Column<?>> data;
+    private Map<Integer, Column<?>> data;
 
-    public Dataframe(List<Column<?>> data){
-        for(int i = 0; i<data.size(); i++)
+    /**
+     * Constructs a Dataframe with the given list of columns.
+     * Each column is assigned an index in the map.
+     *
+     * @param data A list of columns to be stored in the Dataframe.
+     */
+    public Dataframe(List<Column<?>> data) {
+        for (int i = 0; i < data.size(); i++) {
             this.data.put(i, data.get(i));
+        }
     }
 }
