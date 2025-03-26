8f318c52e2434526ec1d418ae6652b798ef8c4a0
Maxim Frolov <max.fro2021@gmail.com>
2025-03-26 10:39:52 +0100

    fix: removed redundunt Map in Dataframe type

--- pandas-java/src/main/java/bababooeyz/Dataframe.java
+++ pandas-java/src/main/java/bababooeyz/Dataframe.java
@@ -2,14 +2,13 @@ package bababooeyz;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * The Dataframe class represents the Dataframe type used in the pandas library.
  * It is a two-dimensional, with axes labeled rows and columns.
  */
 public class Dataframe {
-    private Map<Integer, Column<?>> data;
+    private List<Column<?>> data;
 
     /**
      * Constructs a Dataframe with the given list of columns.
@@ -18,10 +17,7 @@ public class Dataframe {
      * @param data A list of columns to be stored in the Dataframe.
      */
     public Dataframe(List<Column<?>> data) {
-        this.data = new HashMap<>();
-        for (int i = 0; i < data.size(); i++) {
-            this.data.put(i, data.get(i));
-        }
+        this.data = data;
     }
 
     public void ShowDataFrame(Dataframe df) {
