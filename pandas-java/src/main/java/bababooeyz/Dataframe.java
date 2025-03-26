7f8828081639e738d69381ab6aeca8eb7b81f75f
Matthieu Rua <matthieu.rua@etu.univ-grenoble-alpes.fr>
2025-03-26 08:52:18 +0100

    feat: Added function to show a dataframe + getter and setter

--- pandas-java/src/main/java/bababooeyz/Dataframe.java
+++ pandas-java/src/main/java/bababooeyz/Dataframe.java
@@ -21,4 +21,27 @@ public class Dataframe {
             this.data.put(i, data.get(i));
         }
     }
+
+    public void ShowDataFrame(Dataframe df) {
+        // afficher les colonnes
+        for (int i = 0; i < df.data.size(); i++) {
+            System.out.print(df.data.get(i).getName() + " ");
+        }
+        System.out.println();
+        // afficher les valeurs
+        for (int i = 0; i < df.data.get(0).getValues().size(); i++) {
+            for (int j = 0; j < df.data.size(); j++) {
+                System.out.print(df.data.get(j).getValues().get(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public Map<Integer, Column<?>> getData() {
+        return data;
+    }
+
+    public void setData(Map<Integer, Column<?>> data) {
+        this.data = data;
+    }
 }
