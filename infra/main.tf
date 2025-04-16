207d1086950cc51c3fa9541679b8c942300c57f3
Matthieu Rua <matthieu.rua@etu.univ-grenoble-alpes.fr>
2025-04-16 09:02:56 +0200

    Fix: Added the firewall rule directly to the main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,7 @@ provider "google" {
   zone    = "europe-west1-b"
 }
 
+# Création d'une VM avec Apache installé
 resource "google_compute_instance" "vm_instance" {
   name         = "devops-vm"
   machine_type = "e2-micro"
@@ -17,7 +18,7 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     network = "default"
-    access_config {}  # ← Pour avoir une IP publique
+    access_config {} # Nécessaire pour obtenir une IP publique
   }
 
   metadata_startup_script = <<-EOT
@@ -26,4 +27,29 @@ resource "google_compute_instance" "vm_instance" {
     apt install -y apache2
     systemctl start apache2
   EOT
+
+  # Tag réseau pour appliquer la règle firewall
+  tags = ["http-server"]
+}
+
+# Règle de pare-feu pour autoriser le trafic HTTP
+resource "google_compute_firewall" "default-allow-http" {
+  name    = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  direction     = "INGRESS"
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+# Affichage automatique de l'IP publique
+output "vm_ip" {
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  description = "Adresse IP publique de la VM"
 }
